b80b2baabbc837681c875cce4b1dad20ffe61daa
Will Dean <wd60622@gmail.com>
2024-09-28 09:28:16 -0400

    support for one start

--- conjugate/_beta_geometric.py
+++ conjugate/_beta_geometric.py
@@ -6,8 +6,12 @@ from scipy import stats
 from scipy.special import beta
 
 
-def beta_geometric_pmf(x, a, b):
+def beta_geometric_pmf(x, a, b, one_start: bool = True):
     x, a, b = np.broadcast_arrays(x, a, b)
+
+    if not one_start:
+        x += 1
+
     return beta(a + 1, x + b) / beta(a, b)
 
 
@@ -19,16 +23,26 @@ class beta_geometric:
 
     """
 
-    def __init__(self, a, b):
+    def __init__(self, a, b, one_start: bool = True):
         self.a = a
         self.b = b
+        self.one_start = one_start
 
-    def rvs(self, size: int, random_state=None):
+    def rvs(
+        self,
+        size: int | None = None,
+        random_state: np.random.Generator | None = None,
+    ):
         a, b = np.broadcast_arrays(self.a, self.b)
         size = np.broadcast_shapes(a.shape, b.shape, size)
 
         p = stats.beta.rvs(a, b, size=size, random_state=random_state)
-        return stats.geom.rvs(p=p, size=size, random_state=random_state)
+        result = stats.geom.rvs(p=p, size=size, random_state=random_state)
+
+        if not self.one_start:
+            result -= 1
+
+        return result
 
     def pmf(self, x):
-        return beta_geometric_pmf(x, self.a, self.b)
+        return beta_geometric_pmf(x, self.a, self.b, one_start=self.one_start)
