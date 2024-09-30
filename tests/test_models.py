bdaff529878aa84eff6005c6b74a3f7ec81edfee
Will Dean <wd60622@gmail.com>
2024-09-30 04:50:19 -0400

    remove the warning

--- tests/test_models.py
+++ tests/test_models.py
@@ -38,7 +38,6 @@ from conjugate.models import (
     bernoulli_beta_predictive,
     beta,
     binomial_beta,
-    binomial_beta_posterior_predictive,
     exponential_gamma,
     exponential_gamma_predictive,
     gamma,
@@ -619,18 +618,6 @@ def test_hypergeometric_beta_binomial() -> None:
         alpha=6,
         beta=6,
     )
-
-
-def test_old_model_raises_deprecation_warning() -> None:
-    beta = Beta(alpha=1, beta=1)
-    match = "This function is deprecated"
-    with pytest.warns(DeprecationWarning, match=match):
-        predictive = binomial_beta_posterior_predictive(
-            n=10,
-            distribution=beta,
-        )
-
-    assert isinstance(predictive, BetaBinomial)
 
 
 def test_old_parameter_raises_deprecation_warning_model() -> None:
